20e5479d3ad12414449f44d0fbd0ba405d71a7bb
Jack Smolny <evg.sharloimov@gmail.com>
2021-02-09 12:01:30 +0200

    complete readme / comments / config / structure

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ const Image = require("@11ty/eleventy-img");
 const svg = require("@jamshop/eleventy-plugin-svg");
 const sitemap = require("@quasibit/eleventy-plugin-sitemap");
 
+/**
+ * Получаем формат / форматы картинки для функции getImageShortcode
+ * @param {string} src
+ */
 const getImageFormatFromSrc = (src) => {
   const format = path.extname(src).split(".")[1];
 
@@ -13,55 +17,78 @@ const getImageFormatFromSrc = (src) => {
       return ["jpeg", "webp"];
     case "webp":
       return ["webp"];
+    case "avif":
+      return ["avif"];
     case "png":
       return ["png", "webp"];
   }
-}
+};
 
-const getImageShortcode = (src, cs, alt, title) => {
-  const source = path.join(__dirname, "src/images/" , src);
+/**
+ * Получаем html сгенерированную строку для подключения с помощью 11ty-shortcode
+ * @param {string} src
+ * @param {string} className
+ * @param {string} alt
+ * @param {string} title
+ */
+const getImageShortcode = (src, className = "", alt = "", title = "") => {
+  const source = path.join(__dirname, "src/images/", src);
   const format = getImageFormatFromSrc(src);
   const options = {
     widths: [null],
     formats: format,
     outputDir: "./build/images/",
     urlPath: "./images/",
-    sharpOptions: {
-      quality: 80,
-      progressive: true
+    sharpPngOptions: {
+      quality: 100,
+      progressive: true,
+    },
+    sharpJpegOptions: {
+      quality: 100,
+      progressive: true,
+    },
+    sharpWebpOptions: {
+      quality: 100,
+      progressive: true,
+    },
+    sharpAvifOptions: {
+      quality: 100,
+      progressive: true,
     },
     filenameFormat: (id, src, width, format, options) => {
       const extension = path.extname(src);
       const name = path.basename(src, extension);
-  
+
       return `${name}.${format}`;
-    }
+    },
   };
   const image_attributs = {
-    class: cs || "",
-    alt: alt || "",
-    title: title || "",
+    class: className,
+    alt: alt,
+    title: title,
     loading: "lazy",
     decoding: "async",
-    whitespaceMode: "inline"
+    whitespaceMode: "inline",
   };
   const metadata = Image.statsSync(source, options);
 
   Image(source, options);
 
   return Image.generateHTML(metadata, image_attributs);
-}
+};
 
-module.exports = function(config) {
+module.exports = function (config) {
+  // Определяем имя [image / svg / sitemap] для дальнейшего использования в 11ty-shortcode
   config.addNunjucksShortcode("image", getImageShortcode);
-
+  // Добавляем плагин для генерации инлайновых / оптимизированных svg с помощью 11ty-shortcode
   config.addPlugin(svg, { input: "src/images/svg" });
+  // Добавляем плагин для генерации sitemap в выходной папке. Обязательно указываем hostname для генерации правильных url в файле sitemap.html
   config.addPlugin(sitemap, {
     sitemap: {
-      hostname: "https://11ty-starter.com",
+      hostname: "https://localhost:3000",
     },
   });
-
+  // Оптимизация выходных html файлов в raw вид
   config.addTransform("htmlmin", (content, outputPath) => {
     if (outputPath.endsWith(".html")) {
       return htmlmin.minify(content, {
@@ -73,30 +100,32 @@ module.exports = function(config) {
 
     return content;
   });
-
+  // Добавляем 100мс задержки при работе 11ty-watch, нужно что-бы при частых обновлениях кода не выбило call-stack-count ошибку
   config.setWatchThrottleWaitTime(100);
-  
-  config.addPassthroughCopy({ "src/scss/libs": "css/lib" });
-  config.addPassthroughCopy({ "src/js/libs": "js/lib" });
-  config.addPassthroughCopy( "src/manifest.json" );
-  config.addPassthroughCopy( "src/browserconfig.xml" );
-  config.addPassthroughCopy({ "src/images/favicon": "images/favicon" });
-  config.addPassthroughCopy({ "src/fonts": "fonts" });
-  config.addPassthroughCopy({ "src/video": "video" });
-
+  // Определяем папки / пути для копирования в выходную папку build
+  config.addPassthroughCopy({ "src/scss/libs": "css/lib" }); // CSS - библиотеки используемые для разработки
+  config.addPassthroughCopy({ "src/js/libs": "js/lib" }); // JS - библиотеки используемые для разработки
+  config.addPassthroughCopy("src/manifest.json"); // Файл manifest нужен для PWA рейтинга
+  config.addPassthroughCopy({ "src/images/favicon": "images/favicon" }); // Favicon - папка в которую генерируются иконки для PWA рейтинга
+  config.addPassthroughCopy({ "src/fonts": "fonts" }); // Fonts - папка с шрийтами используемыми для разработки
+  config.addPassthroughCopy({ "src/video": "video" }); // Video - папка с видео используемыми для разработки
+  // Определяем папки [pages / includes / layouts / data / scss / js] для 11ty-watch наблюдения
   config.addWatchTarget("src/pages/");
-  config.addWatchTarget("src/templates/");
+  config.addWatchTarget("src/includes/");
+  config.addWatchTarget("src/layouts/");
+  config.addWatchTarget("src/data/");
   config.addWatchTarget("src/scss/**/*.scss");
   config.addWatchTarget("src/js/**/*.js");
 
   return {
     dir: {
-      input: "src",
-      output: "build",
-      includes: "templates",
-      data: "../data",
+      input: "src", // Папка входного каталога
+      output: "build", // Папка выходного каталога
+      includes: "includes", // Папка подключаемых в разработке компонентов
+      layouts: "layouts", // Папка подключаемых в разработке теплейтов
+      data: "data", // Папка подключаемых в разработке json файлов с данными
     },
-    htmlTemplateEngine: "njk",
-    templateFormats: ["html", "njk"],
+    htmlTemplateEngine: "njk", // Определяем ядро обработчика для темплейтов / компонентов
+    templateFormats: ["html", "njk"], // Определяем формат файлов используемых темплейтов
   };
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,51 @@
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const plumber = require('gulp-plumber');
-const scss = require('gulp-sass');
-const uglify = require('gulp-uglify-es').default;
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const plumber = require("gulp-plumber");
+const scss = require("gulp-sass");
+const uglify = require("gulp-uglify-es").default;
 const babel = require("gulp-babel");
 
+// jsBuild - оптимизация / копирование используемых js файлов для разработки
 const jsBuild = () => {
-    return gulp.src('src/js/*.js')
-      .pipe(plumber())
-      .pipe(
-        babel({
-          presets: ["@babel/env"],
-        })
-    )
-      .pipe(uglify())
-      .pipe(gulp.dest('build/js'))
-};
-
-const cssBuild = () => {
-    return gulp.src('src/scss/pages/*.scss')
+  return gulp
+    .src("src/js/*.js")
     .pipe(plumber())
-    .pipe(scss({outputStyle: 'compressed'}).on("error", scss.logError))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('build/css/'))
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      }),
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"));
 };
 
+const jsLibsCopy = () => {
+  return gulp.src("src/js/libs").pipe(plumber()).pipe(gulp.dest("build/js"));
+};
+
+// cssBuild - обработка / оптимизация / копирование используемых scss файлов для разработки
+const cssBuild = () => {
+  return gulp
+    .src("src/scss/pages/*.scss")
+    .pipe(plumber())
+    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(gulp.dest("build/css/"));
+};
+
+const cssLibsCopy = () => {
+  return gulp.src("src/scss/libs").pipe(plumber()).pipe(gulp.dest("build/css"));
+};
+
+// watch - определяем наблюдаемые типы файлов и функции для выполнения при изменении файла
 const watcher = () => {
-    gulp.watch('src/scss/**/*.scss', gulp.series(cssBuild));
-    gulp.watch('src/js/**/*.js', gulp.series(jsBuild));
+  gulp.watch("src/scss/**/*.scss", gulp.series(cssBuild));
+  gulp.watch("src/js/**/*.js", gulp.series(jsBuild));
+  gulp.watch("src/scss/libs/**/*.*", gulp.series(cssLibsCopy));
+  gulp.watch("src/js/libs/**/*.*", gulp.series(jsLibsCopy));
 };
 
+// экспортируем команду обработчика dev
 exports.dev = gulp.series(watcher);
-
+// экспортиируем команду обработчика production
 exports.production = gulp.series(jsBuild, cssBuild);
